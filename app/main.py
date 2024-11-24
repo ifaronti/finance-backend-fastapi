d789ab4829437a3e71dae933aa7fe69181ae0db5
ifaronti <135629496+ifaronti@users.noreply.github.com>
2024-11-24 15:27:46 +0000

    still configuring for vercel deploy. aws after this one fails

--- app/main.py
+++ app/main.py
@@ -4,10 +4,20 @@ from .routers import transactions
 from .routers import budgets
 from .routers import pots
 from .routers import bills
+from prisma import Prisma
 # from mangum import Mangum
 
+prisma = Prisma()
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def prisma_connect():
+    await prisma.connect()
+
+@app.on_event("shutdown")
+async def prisma_disconnect():
+    await prisma.disconnect()
 
 app.include_router(auth.router)
 app.include_router(transactions.router)
