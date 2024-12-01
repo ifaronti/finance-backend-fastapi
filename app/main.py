f0400d726471e839d9798aa60fcea44b2fc97a11
ifaronti <135629496+ifaronti@users.noreply.github.com>
2024-12-01 21:43:02 +0000

    updated schema and main file cleanup

--- app/main.py
+++ app/main.py
@@ -6,9 +6,6 @@ from .routers import pots
 from .routers import bills
 from prisma import Prisma
 from .controllers.account_summary import account_summary
-import json
-
-# from mangum import Mangum
 
 app = FastAPI()
 prisma = Prisma()
@@ -19,13 +16,4 @@ app.include_router(budgets.router)
 app.include_router(pots.router)
 app.include_router(bills.router)
 
-
-# @app.get('/category')
-# async def create_categories():
-#     with open("placeholders.json") as file:
-#         data = json.load(file)
-    
-#     await prisma.connect()
-#     await prisma.category.create_many(data=data["categories"])
-#     await prisma.disconnect()
     
